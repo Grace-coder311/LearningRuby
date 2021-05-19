92676122b2ebdc8ad7fc54cdada6f88dfda8039f
Grace-coder311 <workman.1285@osu.edu>
2021-05-19 19:46:06 -0400

    run and it works - 1st challenge complete

--- LetterGrade.rb
+++ LetterGrade.rb
@@ -6,14 +6,15 @@ Challenge Idea: https://github.com/paircolumbus/LetsDrill
 def get_letter_grade(numeric_grade)
     if numeric_grade >= 90
         return 'A'
-    if else numeric_grade >= 80
+    elsif numeric_grade >= 80
         return 'B'
-    if else numeric_grade >= 70
+    elsif numeric_grade >= 70
         return 'C'
-    if else numeric_grade >= 60
+    elsif numeric_grade >= 60
         return 'D'
     end
     'F' #default return value for <60
 end
 
-#puts 'get_letter_grade(90) => returns #{get_letter_grade(90)}' 
+puts 'get_letter_grade(90) => returns ' + get_letter_grade(90)
+puts 'get_letter_grade(74) => returns ' + get_letter_grade(74)
