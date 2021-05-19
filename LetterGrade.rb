b9e08bf904adacf7ad96e28af02a296675d937ef
Grace-coder311 <workman.1285@osu.edu>
2021-05-19 19:15:15 -0400

    completed the contents of the method - now time for testing

--- LetterGrade.rb
+++ LetterGrade.rb
@@ -1,3 +1,19 @@
+=begin
+Author: Grace Workman
+Challenge Idea: https://github.com/paircolumbus/LetsDrill    
+=end
+    
 def get_letter_grade(numeric_grade)
-
+    if numeric_grade >= 90
+        return 'A'
+    if else numeric_grade >= 80
+        return 'B'
+    if else numeric_grade >= 70
+        return 'C'
+    if else numeric_grade >= 60
+        return 'D'
+    end
+    'F' #default return value for <60
 end
+
+#puts 'get_letter_grade(90) => returns #{get_letter_grade(90)}' 
