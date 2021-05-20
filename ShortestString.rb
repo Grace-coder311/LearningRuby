52cfaa742172868451abc0b8b9e27d93edc0d415
Grace-coder311 <workman.1285@osu.edu>
2021-05-19 20:37:58 -0400

    yee 2

--- ShortestString.rb
+++ ShortestString.rb
@@ -1,6 +1,9 @@
 =begin
     Author: Grace Workman
-    Challenge Idea: https://github.com/paircolumbus/LetsDrill    
+    Challenge Idea: https://github.com/paircolumbus/LetsDrill  
+    
+    shortest_string(['apple', 'car', 'yo']) => returns "yo"
+    shortest_string(['fargo', 'columbus', 'Indianapolis']) => returns "fargo"
 =end
 
 def shortest_string
