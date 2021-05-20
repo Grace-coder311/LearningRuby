ad2ad520451fc0fa20f3874d89ef9dc5f9cfb3ba
Grace-coder311 <workman.1285@osu.edu>
2021-05-20 17:31:14 -0400

    shortest string method completed [1st challenge done]

--- ShortestString.rb
+++ ShortestString.rb
@@ -6,4 +6,15 @@
     shortest_string(['fargo', 'columbus', 'Indianapolis']) => returns "fargo"
 =end
 
-def shortest_string
+def shortest_string(str_array)
+    shortest = str_array[0];
+    str_array.length.times{ |index|
+        if str_array[index].length < shortest.length
+            shortest = str_array[index]
+        end
+    }
+    return shortest
+end
+
+puts 'The shortest string from [apple, car, yo]: ' + shortest_string(['apple', 'car', 'yo'])
+puts 'The shortest string from [fargo, columbus, Indianapolis]: ' + shortest_string(['fargo', 'columbus', 'Indianapolis'])
